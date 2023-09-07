e974eab8530a484e9fcdc1fc064a8091e5b2032d
zyt <zhangyutong04@baidu.com>
2023-09-07 18:20:29 +0800

    One-Click Start

--- training/benchmarks/retinanet/pytorch/model/__init__.py
+++ training/benchmarks/retinanet/pytorch/model/__init__.py
@@ -5,6 +5,6 @@ import os
 
 def create_model():
     torchvision.models.resnet.__dict__['model_urls'][
-            'resnet50'] = 'https://download.pytorch.org/models/resnet50-0676ba61.pth'
+        'resnet50'] = 'https://download.pytorch.org/models/resnet50-0676ba61.pth'
     return retinanet_resnet50_fpn()
 
