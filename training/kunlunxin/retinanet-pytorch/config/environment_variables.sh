8d8a496b712ba14a7eb4c5187343640a8e808906
zyt <zhangyutong04@baidu.com>
2023-09-07 18:36:29 +0800

    One-Click Start

--- training/kunlunxin/retinanet-pytorch/config/environment_variables.sh
+++ training/kunlunxin/retinanet-pytorch/config/environment_variables.sh
@@ -5,13 +5,6 @@ export XMLIR_D_XPU_L3_SIZE=66060288
 export XMLIR_D_FORCE_FALLBACK_STR="aten::_index_put_impl_,aten::index.Tensor"
 export XACC_ARGS="-L auto_tune"
 
-pip install psutil==5.9.5
-pip install accelerate==0.20.3
-pip install Pillow
-pip install torchvision==0.13.1
-pip install pycocotools==2.0.5
-pip install --upgrade numpy==1.23.5
-
 pip uninstall -y xmlir || true
 pip install http://10.1.2.158:8111/flagperf/202307/retinanet/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl
 pip uninstall -y xacc || true
