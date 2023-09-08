2ca99233cbff471a9c29ccaed9a6025a10598b07
zyt <zhangyutong04@baidu.com>
2023-09-08 14:45:45 +0800

    One-Click start

---
+++ training/kunlunxin/retinanet-pytorch/config/config_R300x1x1.py
@@ -0,0 +1,6 @@
+from config_common import *
+
+train_batch_size = 6
+eval_batch_size = 4
+
+lr = 0.006
--- training/kunlunxin/retinanet-pytorch/config/environment_variables.sh
+++ training/kunlunxin/retinanet-pytorch/config/environment_variables.sh
@@ -1,14 +1,6 @@
 export XACC_ENABLE=1
 export BKCL_PCIE_RING=1
-export BKCL_FORCE_SYNC=1
 export XMLIR_D_XPU_L3_SIZE=66060288
 export XMLIR_D_FORCE_FALLBACK_STR="aten::_index_put_impl_,aten::index.Tensor"
-export XACC_ARGS="-L auto_tune"
-
-pip uninstall -y xmlir || true
-pip install http://10.1.2.158:8111/flagperf/202307/retinanet/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl
-pip uninstall -y xacc || true
-pip install http://10.1.2.158:8111/flagperf/202307/retinanet/xacc-0.1.0-cp38-cp38-linux_x86_64.whl
-python -m xacc.install
-
+export XACC_ARGS="-L auto_tune -L mmcv_ext"
 
